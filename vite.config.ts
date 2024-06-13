91962374f8b9142b1091aea5d4e9f5f726640416
Egharevba Nosa <96873980+Nosa-Drexx@users.noreply.github.com>
2024-06-13 10:11:51 +0100

    more package setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { resolve } from 'node:path'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cssInjectedByJsPlugin(), dts()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -17,7 +19,7 @@ export default defineConfig({
       // src/indext.ts is where we have exported the component(s)
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'BottomSheet',
-      fileName: (format) => `${format}.js`
+      fileName: (format: string) => `vue-bottom-sheet.${format}.js`
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
